3c10830bff10e9c3efa84f0852168ed35d868cdb
Andreas Ragen Ayal <andreas.ayal@ut.ee>
2017-07-29 11:55:43 +0300

    A new beginning

---
+++ src/main/java/com/example/models/PlantInventoryEntryRepository.java
@@ -0,0 +1,7 @@
+package com.example.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlantInventoryEntryRepository extends JpaRepository<PlantInventoryEntry, Long>
+{
+}
